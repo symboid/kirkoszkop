fa24a9b671094d62846d5b641687f6bec98a6730
Török Róbert <torok.robert@symboid.com>
2021-03-11 12:58:40 +0100

    Update defs.h

--- app/defs.h
+++ app/defs.h
@@ -2,6 +2,6 @@
 #ifndef __SYMBOID_KIRKOSZKOP_APP_DEFS_H__
 #define __SYMBOID_KIRKOSZKOP_APP_DEFS_H__
 
-#include "sdk/defs.h"
+#include "sdk/arch/defs.h"
 
 #endif // __SYMBOID_KIRKOSZKOP_APP_DEFS_H__
